8e9b62a0b62b27bf861455addeb205d9198af114
nikitakot <kotn01@outlook.cz>
2018-11-18 23:34:04 +0100

    fix link

--- src/components/LocalizedLink.js
+++ src/components/LocalizedLink.js
@@ -3,6 +3,8 @@ import { Link } from 'gatsby'
 import { injectIntl } from 'react-intl'
 
 const LocalizedLink = ({ to, intl: { locale }, ...props }) => {
+  if(locale === 'uk') locale = 'ua'
+
   const path = `/${locale}${to}`
 
   return <Link {...props} to={path} />
